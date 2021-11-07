28007d1a4517c1fbcf346f5201a6d0941a22b5cd
xiaqiubo <xiaqb01@mingyuanyun.com>
2021-11-07 14:39:00 +0800

    [feat]增加基础函数

--- text/util.go
+++ text/util.go
@@ -1,13 +1,46 @@
 package text
 
-func Length(src string) int32 {
+import (
+	"unicode/utf16"
+	"unicode/utf8"
+	"unsafe"
+)
 
+type charType string
+
+const (
+	CharTypeASCII charType = "ascii"
+	CharTypeUTF8           = "utf8"
+	CharTypeUTF16          = "utf16"
+	CharTypeGBK            = "gbk"
+)
+
+func NewCharType(tp string) charType {
+	return charType(tp)
 }
 
-func IndexOf(src, search string) int32 {
+func Length(src string, tps ...charType) int {
+	var tp charType = CharTypeUTF8
+	if len(tps) > 0 {
+		tp = tps[0]
+	}
 
+	switch tp {
+	case CharTypeASCII:
+		return len(src)
+	case CharTypeUTF8:
+		return utf8.RuneCountInString(src)
+	case CharTypeUTF16:
+		return len(utf16.Encode([]rune(src)))
+	default:
+		return len(src)
+	}
 }
 
-func Repalce(src, search, repalce string) int32 {
+func Bytes(src string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&src))
+}
 
+func String(bt []byte) string {
+	return *(*string)(unsafe.Pointer(&bt))
 }
